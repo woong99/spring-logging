06cdfda3700f69d12394431e7d0d6fdbb1493cc2
Woong99 <dndry1072@naver.com>
2025-09-28 22:32:32 +0900

    feat: log4j2 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,27 @@ repositories {
     mavenCentral()
 }
 
+configurations {
+    // log4j2를 위한 설정
+    configureEach {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+}
+
 dependencies {
+    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // log4j2
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0")
+
+    // Kotlin-Logging
+    runtimeOnly("io.github.oshai:kotlin-logging-jvm:7.0.13")
+
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
